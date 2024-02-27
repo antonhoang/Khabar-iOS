be8755e3aa23bcd20a01238955395779aeceda9e
antonhoang <hoang.anton@fintech-farm.com>
2024-02-27 19:09:41 +0100

    add board optimizations

--- Il2CppOutputProject/Source/il2cppOutput/Unity.Mathematics_CodeGen.c
+++ Il2CppOutputProject/Source/il2cppOutput/Unity.Mathematics_CodeGen.c
@@ -446,14 +446,14 @@ static const int32_t s_InvokerIndices[111] =
 };
 static TypeDefinitionIndex s_staticConstructorsToRunAtStartup[9] = 
 {
-	4412,
-	4414,
+	4411,
+	4413,
+	4415,
 	4416,
-	4417,
-	4419,
-	4421,
-	4423,
-	4425,
+	4418,
+	4420,
+	4422,
+	4424,
 	0,
 };
 IL2CPP_EXTERN_C const Il2CppCodeGenModule g_Unity_Mathematics_CodeGenModule;
